1c6be123c8491c04e5814d17c2bc2d01d560d907
VanderSant <vanderson.santos@usp.br>
2021-02-21 18:33:09 -0300

    :sparkles: adição de objeto coadjuvante

--- libs/inc/coadjuvante.h
+++ libs/inc/coadjuvante.h
@@ -1,49 +1,52 @@
-#ifndef coadjuvante
-#define coadjuvante
+/**
+ * @file  coadjuvante.h
+ *
+ * @brief Virtual version of onix
+ *
+ *
+ * @author Gabriel Kishida <gabriel.kishida@usp.br>
+ * @author Henrique Matheus <henriquedamaralhdm@usp.br>
+ * @author Lucas Guedes <guedeslucas@usp.br>
+ * @author Vanderson Santos <vanderson.santos@thunderatz.org>
+ *
+ * @date 02/2021
+ */
 
+#include <Arduino.h>
 #include <stdlib.h>     /* srand, rand */
 #include <time.h>       /* time */
 
-class coadjuvante {
-  coadjuvante(); 
-  
-  public:
-    int readDPI();
-    void setup_coadj();
-    int random_angle();
-    
-    int time_ms;
-    int turnAngle = 0;
- 
-}
+#ifndef coadjuvante
+#define coadjuvante
+
 
 //defines
-
 #define DIP1 10
 #define DIP2 11
 #define DIP3 12
 #define DIP4 13
-
 #define LED 6
 
 //timer
 #define reset_timer() (time_ms = millis())
 #define get_timer() (millis() - time_ms)
 
-//PUBLIC FUNCTIONS
 
-coadjuvante(){
-};
-
-int coadjuvante::readDPI();
-
-void coadjuvante::setup_coadj();
-
-int coadjuvante::random_angle();
-
-
-
-
+class coadjuvante {
+  
+  public:
+    //variaveis
+    int time_ms;
+    int turnAngle = 0;
+    
+    //funções
+    coadjuvante();
+    ~coadjuvante(); 
+    int readDPI();
+    int random_angle();
+    void setup_coadj();
+    
+}
 
 
 #endif
